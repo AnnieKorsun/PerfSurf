b6c53827a9d4e80eb83e4d60d452726cbbc6d6ce
Anastasiia KORSUN (contractor) <anastassia.korsun@amadeus.com>
2018-01-26 15:55:46 +0100

    fixing Windows auth

--- PerfSurf/Hubs/PerfHub.cs
+++ PerfSurf/Hubs/PerfHub.cs
@@ -4,9 +4,9 @@ namespace PerfSurf.Hubs
 {
     public class PerfHub : Hub
     {
-        public void Hello()
+        public void Send(string message)
         {
-            Clients.All.hello("Hi!");
+            Clients.All.NewMessage(Context.User.Identity.Name + " says " + message);
         }
     }
 }
--- PerfSurf/Scripts/perfSurf.js
+++ PerfSurf/Scripts/perfSurf.js
@@ -1,1 +1,35 @@
-﻿
+﻿(function() {
+    var perfHub = $.connection.perfHub;
+    $.connection.hub.logging = true;
+    $.connection.hub.start();
+
+    var Model = function () {
+        var self = this;
+        self.message = ko.observable("");
+        self.messages = ko.observableArray();
+    };
+
+    var model = new Model();
+
+    perfHub.clientFailure.NewMessage = function(message) {
+        model.addMessage(message);
+    }
+    
+    Model.prototype = {
+
+        sendMessage: function() {
+            var self = this;
+            perfHub.server.send(self.message());
+            self.message("");
+        },
+
+        addMessage: function(message) {
+            var self = this;
+            self.messages.push(message);
+        }
+    };
+
+    $(function() {
+        ko.applyBindings(model);
+    });
+})
