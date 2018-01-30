bd6da8501c0879a48ae9625e1b5fb959f615c21d
Anastasiia KORSUN (contractor) <anastassia.korsun@amadeus.com>
2018-01-30 10:46:35 +0100

    adding performance counters

---
+++ PerfSurf/Counters/PerfCounterWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace PerfSurf.Counters
+{
+    public class PerfCounterWrapper
+    {
+        private readonly PerformanceCounter _counter;
+
+        public string Name { get; set; }
+
+        public float Value
+        {
+            get { return _counter.NextValue(); }
+        }
+
+        public PerfCounterWrapper(string name, string category, string counter, string instance = "")
+        {
+            Name = name;
+            _counter= new PerformanceCounter(category, counter, instance, readOnly: true);
+        }
+    }
+}
--- PerfSurf/Hubs/PerfHub.cs
+++ PerfSurf/Hubs/PerfHub.cs
@@ -1,9 +1,30 @@
-﻿using Microsoft.AspNet.SignalR;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using PerfSurf.Counters;
 
 namespace PerfSurf.Hubs
 {
     public class PerfHub : Hub
     {
+        public PerfHub()
+        {
+            StartCounterCollection();
+        }
+
+        private void StartCounterCollection()
+        {
+            var task = Task.Factory.StartNew(async () =>
+            {
+                var perfService = new PerformanceCounterService();
+                while (true)
+                {
+                    var results = perfService.GetResults();
+                    Clients.All.NewCounters(results);
+                    await Task.Delay(2000);
+                }
+            }, TaskCreationOptions.LongRunning);
+        }
+
         public void Send(string message)
         {
             Clients.All.NewMessage(Context.User.Identity.Name + " says " + message);
